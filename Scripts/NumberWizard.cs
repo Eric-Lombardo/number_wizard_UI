33998ddb7d1cc3506be78d5842c8d40097ac26e7
Eric Lombardo <ejlanim8or@gmail.com>
2020-04-29 16:25:38 -0400

    add logic for higher/lower button onClicks

---
+++ Scripts/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int high;
+    [SerializeField] int low;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        guess = (low + high) / 2;
+        guessText.text = guess.ToString();
+        high = high + 1;
+    }
+
+    public void OnPressHigher()
+    {
+        low = guess;
+        CalculateNewGuess();
+    }
+
+    public void OnPressLower()
+    {
+        high = guess;
+        CalculateNewGuess();
+    }
+
+    void CalculateNewGuess() 
+    {
+        guess = (low + high) / 2;
+        guessText.text = guess.ToString();
+    }
+
+
+}
